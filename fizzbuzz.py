0751e0163b0626b0b17e52375d9583d909766ee6
Anton Višnevski <antonvisnevski@Antons-MacBook-Air.local>
2021-01-14 01:24:29 +0200

    Modification of fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,41 +1,15 @@
 def fizzbuzz(x):
-	if x % 15 == 0:
-		return("fizzbuzz")
-	elif x % 3 == 0:
-		return("fizz")
-	elif x % 5 == 0:
-		return("buzz")
+	array = ""
+	if x % 3 == 0:
+		array = "fizz"
+	if x % 5 == 0:
+		array += "buzz"
+	if x % 3 != 0 and x % 5 != 0:
+		print(x) 
+		return(x)
+	else:
+		print(array)
+		return(array)
 
-
-
-if fizzbuzz(15) == "fizzbuzz":
-	print("Test1 passed")
-else:
-	print("Test1 failed")
-
-if fizzbuzz(5) == "buzz":
-	print("Test2 passed")
-else:
-	print("Test2 failed")
-
-if fizzbuzz(3) == "fizz":
-	print("Test3 passed")
-else:
-	print("Test3 failed")
-
-
-test4array = ""
-for x in range(1,31):
-	test4array += str(fizzbuzz(x))
-if test4array.count("fizz") == 10:
-	print("Test4 passed")
-else:
-	print("Test4 failed") 
-
-test5array = ""
-for x in range(1,31):
-	test5array += str(fizzbuzz(x))
-if test5array.count("buzz") == 6:
-	print("Test5 passed")
-else:
-	print("Test5 failed") 
+for x in range(1,101):
+	fizzbuzz(x)
